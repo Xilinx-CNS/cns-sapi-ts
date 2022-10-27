28fe219dbd06a65af81144ba7cb54a51906bcfb4
Yurij Plotnikov <yurij.plotnikov@arknetworks.am>
2022-10-27 19:34:05 +0400

    scripts: add guessing of TE_TS_RIGSDIR
    After splitting ts-conf into to repos we need to guess the second repo
    and save it in this variable.
    
    Signed-off-by: Yurij Plotnikov <yurij.plotnikov@arknetworks.am>
    Reviewed-by: Denis Pryazhennikov <denis.pryazhennikov@arknetworks.am>

--- scripts/guess.sh
+++ scripts/guess.sh
@@ -50,6 +50,22 @@ done
 export SF_TS_CONFDIR=$(te_realpath ${SF_TS_CONFDIR})
 echo "SF_TS_CONFDIR=${SF_TS_CONFDIR}"
 
+# TE_TS_RIGSDIR
+for loop in once ; do
+    test -d "${TE_TS_RIGSDIR}" && break;
+
+    echo "Guessing TE_TS_RIGSDIR"
+    export TE_TS_RIGSDIR="${GUESS_TOP_DIR}"/../ts-env
+    test -d "${TE_TS_RIGSDIR}" && break;
+    export TE_TS_RIGSDIR="${GUESS_TOP_DIR}"/../ts-rigs
+    test -d "${TE_TS_RIGSDIR}" && break;
+
+    echo "Cannot import SF shared TS confdir '$TE_TS_RIGSDIR'" >&2
+    exit 1
+done
+export TE_TS_RIGSDIR=$(te_realpath ${TE_TS_RIGSDIR})
+echo "TE_TS_RIGSDIR=${TE_TS_RIGSDIR}"
+
 # SFC_ONLOAD_LIB
 for loop in once ; do
     test -d "${SFC_ONLOAD_LIB}" && break;
