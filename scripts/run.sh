Xilinx-CNS/cns-sapi-ts

commit d74da290255c598bb92511ef3a8dd4cba0b57caf

scripts: run sockapi configuration scripts last
Run sockapi configuration scripts after the scripts fixing ool
consistency, because different branches may contain different
set of extension headers.

OL-Redmine-Id: 13098
Signed-off-by: Alexander Kuzminov <alexander.kuzminov@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>
Reviewed-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>

scripts/run.sh
@@ -232,10 +232,6 @@ if test "${ZF_SHIM_RUN}" = "true" ; then
     check_sf_zetaferno_dir $OOL_SET
 fi
 
-if test -n "${TE_TS_SOCKAPI}" ; then
-    RUN_OPTS="${RUN_OPTS} --opts=opts.ts"
-fi
-
 if test "${ST_IP_TRANSPARENT}" = "yes" ; then
     RUN_OPTS="${RUN_OPTS} --tester-req=\"ENV-IUT-FAKE-ADDR\""
     # Scalable filters are not supported with AF_XDP. ST-2231.
@@ -299,6 +295,10 @@ if [[ "$TE_TS_BUILD_ONLY" != "yes" ]] ; then
             RUN_OPTS="$RUN_OPTS --script=ool/config/$i"
         fi
     done
+fi
+
+if test -n "${TE_TS_SOCKAPI}" ; then
+    RUN_OPTS="${RUN_OPTS} --opts=opts.ts"
 fi
 
 eval "${TE_BASE}/dispatcher.sh ${MY_OPTS} ${RUN_OPTS}"

commit 14d5aee6aa19e1170fa7ef5d556c9b1d4acd8e83

scripts: copy branch specific headers set last
Copy the set of headers, specific for currently used branch, or create
symlinks for them in the script run last.

Run other sockapi configurations before the scripts fixing ool
consistency as further executed scripts need exports from sockapi
configurations.

Fixes: d74da290255c ("scripts: run sockapi configuration scripts last")
OL-Redmine-Id: 13098
Signed-off-by: Alexander Kuzminov <alexander.kuzminov@oktetlabs.ru>
Reviewed-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>

conf/scripts/copy-onload-headers
@@ -1,0 +1,30 @@
+# SPDX-License-Identifier: Apache-2.0
+# (c) Copyright 2023 OKTET Labs Ltd., 2004 - 2022 Xilinx, Inc. All rights reserved.
+
+# Copy/create symlinks for the set of Onload headers, specific for currently
+# used branch
+for header in extensions.h extensions_zc.h extensions_timestamping.h\
+    extensions_zc_hlrx.h test_intf.h; do
+    dst_name="${SOCKAPI_TS_LIBDIR}/talib_sockapi_ts/${header}"
+    if [[ "$SFC_ONLOAD_LOCAL" == "yes" ]]; then
+        local_file="${SFC_ONLOAD_EXT_HEADERS}/${header}"
+        target="${SOCKAPI_TS_LIBDIR}/talib_sockapi_ts/copied_headers/${header}"
+        for ignored_header in "${IGNORED_ONLOAD_HEADERS[@]}"; do
+            if [[ "${header}" == "${ignored_header}" ]]; then
+                HEADER_IS_IGNORED="yes"
+                break
+            fi
+        done
+        if [[ "${HEADER_IS_IGNORED}" == "yes" ]]; then
+            echo "INFO: file $header does not exist"
+        else
+            rsync_from "$TE_IUT" "$local_file" "$target"
+        fi
+        HEADER_IS_IGNORED=
+    else
+        target="${SFC_ONLOAD_EXT_HEADERS}/${header}"
+    fi
+    if test -f "$target"; then
+        ln_sf_safe "$target" "$dst_name"
+    fi
+done

conf/scripts/sockapi-ts
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
-# (c) Copyright 2004 - 2022 Xilinx, Inc. All rights reserved.
+# (c) Copyright 2023 OKTET Labs Ltd., 2004 - 2022 Xilinx, Inc. All rights reserved.
 #
 # The file contains sockapi-ts specific environments processing and
 # configuration.
@@ -233,31 +233,6 @@ fi
 if test "x$SFC_ONLOAD_LOCAL" == "xyes" ; then
     mkdir -p "${SOCKAPI_TS_LIBDIR}/talib_sockapi_ts/copied_headers"
 fi
-for header in extensions.h extensions_zc.h extensions_timestamping.h extensions_zc_hlrx.h\
-    test_intf.h ; do
-    dst_name="${SOCKAPI_TS_LIBDIR}/talib_sockapi_ts/${header}"
-    if test "x$SFC_ONLOAD_LOCAL" == "xyes" ; then
-        local_file="${SFC_ONLOAD_EXT_HEADERS}/${header}"
-        target="${SOCKAPI_TS_LIBDIR}/talib_sockapi_ts/copied_headers/${header}"
-        for ignored_header in "${IGNORED_ONLOAD_HEADERS[@]}"; do
-            if [[ "${header}" == "${ignored_header}" ]]; then
-                HEADER_IS_IGNORED="yes"
-                break
-            fi
-        done
-        if [[ "${HEADER_IS_IGNORED}" == "yes" ]]; then
-            echo "INFO: file $header does not exist"
-        else
-            rsync_from "$TE_IUT" "$local_file" "$target"
-        fi
-        HEADER_IS_IGNORED=
-    else
-        target="${SFC_ONLOAD_EXT_HEADERS}/${header}"
-    fi
-    if test -f "$target"; then
-        ln_sf_safe "$target" "$dst_name"
-    fi
-done
 
 if test "x$SFC_ONLOAD_LOCAL" == "xyes" ; then
 

scripts/run.sh
@@ -232,6 +232,10 @@ if test "${ZF_SHIM_RUN}" = "true" ; then
     check_sf_zetaferno_dir $OOL_SET
 fi
 
+if test -n "${TE_TS_SOCKAPI}" ; then
+    RUN_OPTS="${RUN_OPTS} --opts=opts.ts"
+fi
+
 if test "${ST_IP_TRANSPARENT}" = "yes" ; then
     RUN_OPTS="${RUN_OPTS} --tester-req=\"ENV-IUT-FAKE-ADDR\""
     # Scalable filters are not supported with AF_XDP. ST-2231.
@@ -297,8 +301,8 @@ if [[ "$TE_TS_BUILD_ONLY" != "yes" ]] ; then
     done
 fi
 
-if test -n "${TE_TS_SOCKAPI}" ; then
-    RUN_OPTS="${RUN_OPTS} --opts=opts.ts"
+if test -n "${TE_TS_SOCKAPI}"; then
+    RUN_OPTS="$RUN_OPTS --script=scripts/copy-onload-headers"
 fi
 
 eval "${TE_BASE}/dispatcher.sh ${MY_OPTS} ${RUN_OPTS}"