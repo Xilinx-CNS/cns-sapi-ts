Xilinx-CNS/cns-sapi-ts

commit 337eeea05eefb6e9338be3682668364b402bffdf

lib: add new library functions for net namespace
Add functions sockts_iut_netns_setup() and sockts_destroy_netns()
to lib/sockapi-ts. These functions create/delete a net namespace,
Test agent within it, with specified interface added to it. They
are used in several new tests.

Signed-off-by: Boris Shleyfman <bshleyfman@oktet.co.il>
Reviewed-by: Yurij Plotnikov <yurij.plotnikov@arknetworks.am>

sockapi-ts/lib/sockapi-ts.c
@@ -3611,3 +3611,102 @@ sockts_find_parent_if(rcf_rpc_server *rpcs,
 {
     sockts_find_parent_if_ext(rpcs, ifname, ifaces, TRUE);
 }
+
+/* See description in sockapi-ts.h */
+void
+sockts_netns_setup_common(const char *ta_name, const char *host,
+                          const char *ta_type, const char *ta_rpcprovider,
+                          cfg_handle net_handle, const char *recv_veth2_name,
+                          const char *netns, const char *netns_ta,
+                          const char *netns_rpcs, uint16_t rcf_port,
+                          rcf_rpc_server **rpcs_ns, struct sockaddr **ns_addr,
+                          cfg_handle *addr_handle)
+{
+    struct sockaddr *addr = NULL;
+
+    CHECK_NOT_NULL(ta_name);
+    CHECK_NOT_NULL(host);
+    CHECK_NOT_NULL(ta_type);
+    CHECK_NOT_NULL(ta_rpcprovider);
+    CHECK_NOT_NULL(recv_veth2_name);
+    CHECK_NOT_NULL(netns);
+    CHECK_NOT_NULL(netns_ta);
+    CHECK_NOT_NULL(netns_rpcs);
+    CHECK_NOT_NULL(rpcs_ns);
+
+    CHECK_RC(tapi_netns_add(ta_name, netns));
+    CHECK_RC(tapi_netns_if_set(ta_name, netns, recv_veth2_name));
+    CHECK_RC(tapi_netns_add_ta(host, netns, netns_ta, ta_type,
+                               rcf_port, NULL, NULL, TRUE));
+    CHECK_RC(cfg_synchronize("/:", TRUE));
+    CHECK_RC(cfg_set_instance_fmt(CVT_STRING, ta_rpcprovider,
+                                  "/agent:%s/rpcprovider:", netns_ta));
+
+    CHECK_RC(tapi_cfg_base_if_add_rsrc(netns_ta, recv_veth2_name));
+    CHECK_RC(tapi_cfg_base_if_up(netns_ta, recv_veth2_name));
+
+    if (ns_addr == NULL)
+        ns_addr = &addr;
+    if (*ns_addr == NULL)
+        CHECK_RC(tapi_cfg_alloc_net_addr(net_handle, addr_handle, ns_addr));
+
+    CHECK_RC(tapi_cfg_base_if_add_net_addr(netns_ta, recv_veth2_name,
+                                           *ns_addr,
+                                           te_netaddr_get_bitsize(AF_INET),
+                                           FALSE, NULL));
+
+    /*
+     * Loopback interface should be UP in the namespace for logging from
+     * RPC servers to work correctly.
+     * TE grabs interface by its name, so it does not allow to grab
+     * "lo" interface in two different namespaces by two different
+     * TAs simultaneously.
+     */
+    CHECK_RC(tapi_cfg_base_if_del_rsrc(ta_name, "lo"));
+    CHECK_RC(tapi_cfg_base_if_add_rsrc(netns_ta, "lo"));
+    CHECK_RC(tapi_cfg_base_if_up(netns_ta, "lo"));
+
+    CHECK_RC(tapi_cfg_base_if_del_rsrc(netns_ta, "lo"));
+    CHECK_RC(tapi_cfg_base_if_add_rsrc(ta_name, "lo"));
+
+    CHECK_RC(rcf_rpc_server_create(netns_ta, netns_rpcs, rpcs_ns));
+    CFG_WAIT_CHANGES;
+}
+
+/* See description in sockapi-ts.h */
+void
+sockts_iut_netns_setup(rcf_rpc_server *pco_iut, cfg_handle net_handle,
+                       const char *recv_veth2_name, const char *netns,
+                       const char *netns_ta, const char *netns_rpcs,
+                       rcf_rpc_server **rpcs_ns, struct sockaddr **ns_addr,
+                       cfg_handle *addr_handle)
+{
+    const char *host;
+    const char *ta_type;
+    const char *ta_rpcprovider;
+    uint16_t rcf_port;
+
+    CHECK_NOT_NULL(host = getenv("TE_IUT"));
+    CHECK_NOT_NULL(ta_type = getenv("TE_IUT_TA_TYPE"));
+    CHECK_NOT_NULL(ta_rpcprovider = getenv("SF_TS_IUT_RPCPROVIDER"));
+
+    CHECK_RC(tapi_allocate_port_htons(pco_iut, &rcf_port));
+
+    sockts_netns_setup_common(pco_iut->ta, host, ta_type, ta_rpcprovider,
+                              net_handle, recv_veth2_name, netns, netns_ta,
+                              netns_rpcs, rcf_port, rpcs_ns, ns_addr,
+                              addr_handle);
+}
+
+/* See description in sockapi-ts.h */
+void
+sockts_destroy_netns(const char *ta, rcf_rpc_server *rpcs_ns,
+                     const char *netns, const char *netns_ta)
+{
+    CHECK_RC(rcf_rpc_server_destroy(rpcs_ns));
+    CHECK_RC(rcf_del_ta(netns_ta));
+    CHECK_RC(tapi_host_ns_agent_del(netns_ta));
+    CHECK_RC(tapi_netns_del(ta, netns));
+
+    CHECK_RC(cfg_synchronize_fmt(TRUE, "/agent:%s", netns_ta));
+}

sockapi-ts/lib/sockapi-ts.h
@@ -59,6 +59,8 @@
 #include "tapi_sh_env.h"
 #include "tapi_tcp_states.h"
 #include "tapi_mem.h"
+#include "tapi_namespaces.h"
+#include "tapi_host_ns.h"
 
 #include "sockapi-ts_rpc.h"
 #include "sockapi-ts_env.h"
@@ -4047,6 +4049,81 @@ sockts_get_used_if_name(rcf_rpc_server *rpcs,
 extern void sockts_find_parent_if(rcf_rpc_server *rpcs,
                                   const char *ifname,
                                   tqh_strings *ifaces);
+
+/**
+ * Add namespace on @p host and @p recv_veth2_name interface to it.
+ * If @p ns_addr is not @c NULL and it doesn't point to @c NULL, use this
+ * address; otherwise, allocate an IP address from @p net_handle. Add the
+ * address to the interface.
+ * Start test agent and RPC server in namespace.
+ *
+ * @param ta_name               Test agent name.
+ * @param host                  Host name.
+ * @param ta_type               Test agent type.
+ * @param ta_rpcprovider        RPC provider.
+ * @param net_handle            Network handle (network addresses pool).
+ * @param recv_veth2_name       Name of the interface to pass to namespace.
+ * @param netns                 Name of the created namespace.
+ * @param netns_ta              Name of the created Test Agent.
+ * @param netns_rpcs            Name of the created RPC server.
+ * @param rcf_port              Port number.
+ * @param rpcs_ns               Location for netns RPC server.
+ * @param ns_addr               Location for allocated address.
+ * @param addr_handle           Location for allocated address configuration
+ *                              handle.
+ *
+ * @note The function jumps to cleanup in case of failure.
+ */
+extern void
+sockts_netns_setup_common(const char *ta_name, const char *host,
+                          const char *ta_type, const char *ta_rpcprovider,
+                          cfg_handle net_handle,
+                          const char *recv_veth2_name, const char *netns,
+                          const char *netns_ta, const char *netns_rpcs,
+                          uint16_t rcf_port, rcf_rpc_server **rpcs_ns,
+                          struct sockaddr **ns_addr,
+                          cfg_handle *addr_handle);
+
+
+/**
+ * Add namespace on IUT host and @p recv_veth2_name interface to it.
+ * Allocate IP address from @p net_handle and add it to interface.
+ * Start test agent and RPC server in namespace.
+ *
+ * @param pco_iut               RPC server in the original namespace.
+ * @param net_handle            Network handle (network addresses pool).
+ * @param recv_veth2_name       Name of the interface to pass to namespace.
+ * @param netns                 Name of the created namespace.
+ * @param netns_ta              Name of the created Test Agent.
+ * @param netns_rpcs            Name of the created RPC server.
+ * @param rpcs_ns               Location for netns RPC server.
+ * @param ns_addr               Location for allocated address.
+ * @param addr_handle           Location for allocated address configuration
+ *                              handle.
+ *
+ * @note The function jumps to cleanup in case of failure.
+ */
+extern void
+sockts_iut_netns_setup(rcf_rpc_server *pco_iut, cfg_handle net_handle,
+                       const char *recv_veth2_name, const char *netns,
+                       const char *netns_ta, const char *netns_rpcs,
+                       rcf_rpc_server **rpcs_ns, struct sockaddr **ns_addr,
+                       cfg_handle *addr_handle);
+
+/**
+ * Destroy the created network namespace together with TA and RPC server
+ * created in it.
+ *
+ * @param ta          Test agent on IUT.
+ * @param rpcs_ns     RPC server in the created namespace.
+ * @param netns       Name of the namespace.
+ * @param netns_ta    Name of the Test Agent.
+ *
+ * @note The function jumps to cleanup in case of failure.
+ */
+extern void
+sockts_destroy_netns(const char *ta, rcf_rpc_server *rpcs_ns,
+                     const char *netns, const char *netns_ta);
 
 #ifdef __cplusplus
 } /* extern "C" */

commit 545586f943eb38a44a3df6403192155486400993

lib: fix network prefix getting in library function
In function sockts_netns_setup_common() in lib/sockapi-ts, get network
prefix based on the network handle passed as the argument of the
function. Use it for adding network address to the interface instead of
te_netaddr_get_bitsize(AF_INET).

Fixes: 337eeea05eef ("lib: add new library functions for net namespace")
Signed-off-by: Boris Shleyfman <bshleyfman@oktet.co.il>
Reviewed-by: Yurij Plotnikov <yurij.plotnikov@arknetworks.am>
Reviewed-by: Denis Pryazhennikov <denis.pryazhennikov@arknetworks.am>

sockapi-ts/lib/sockapi-ts.c
@@ -3636,6 +3636,9 @@ sockts_netns_setup_common(const char *ta_name, const char *host,
                           cfg_handle *addr_handle)
 {
     struct sockaddr *addr = NULL;
+    char            *net_oid = NULL;
+    cfg_val_type     val_type = CVT_INTEGER;
+    unsigned int     net_prefix;
 
     CHECK_NOT_NULL(ta_name);
     CHECK_NOT_NULL(host);
@@ -3662,10 +3665,13 @@ sockts_netns_setup_common(const char *ta_name, const char *host,
     if (*ns_addr == NULL)
         CHECK_RC(tapi_cfg_alloc_net_addr(net_handle, addr_handle, ns_addr));
 
-    CHECK_RC(tapi_cfg_base_if_add_net_addr(netns_ta, recv_veth2_name,
-                                           *ns_addr,
-                                           te_netaddr_get_bitsize(AF_INET),
-                                           FALSE, NULL));
+    /* Get network prefix */
+    CHECK_RC(cfg_get_oid_str(net_handle, &net_oid));
+    CHECK_RC(cfg_get_instance_fmt(&val_type, &net_prefix, "%s/prefix:",
+                                  net_oid));
+
+    CHECK_RC(tapi_cfg_base_if_add_net_addr(netns_ta, recv_veth2_name, *ns_addr,
+                                           net_prefix, FALSE, NULL));
 
     /*
      * Loopback interface should be UP in the namespace for logging from
@@ -3683,6 +3689,8 @@ sockts_netns_setup_common(const char *ta_name, const char *host,
 
     CHECK_RC(rcf_rpc_server_create(netns_ta, netns_rpcs, rpcs_ns));
     CFG_WAIT_CHANGES;
+
+    free(net_oid);
 }
 
 /* See description in sockapi-ts.h */