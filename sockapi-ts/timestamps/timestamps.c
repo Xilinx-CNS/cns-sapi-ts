Xilinx-CNS/cns-sapi-ts

commit 25d5e238467c1bb328479065535f9aafb0390f0e

timestamps: change expected header size without timestamps
In case of TCP with disabled timestamps, msg header size is smaller
by 12 than expected, because tcp header doesn't have Options field
with 12 bytes length, that contains timestamps information. So expected
msg header size was decreased by 12.

AMD-Jira-ID: ST-2722
Signed-off-by: Demid Trempolskii <demid.trempolskii@arknetworks.am>
Reviewed-by: Yurij Plotnikov <yurij.plotnikov@arknetworks.am>

sockapi-ts/timestamps/timestamps.c
@@ -314,6 +314,7 @@ ts_check_cmsghdr_addr(rpc_msghdr *msg, int rc, size_t sent_len,
                       const struct sockaddr *addr, struct timespec *ts_o,
                       struct timespec *ts_prev)
 {
+    char *disable_timestamps = getenv("DISABLE_TIMESTAMPS");
     rpc_onload_scm_timestamping_stream *ts_tx;
     rpc_scm_timestamping *ts;
     struct cmsghdr *cmsg = NULL;
@@ -382,6 +383,19 @@ ts_check_cmsghdr_addr(rpc_msghdr *msg, int rc, size_t sent_len,
         {
             if (tx && vlan)
                 hsize += 4;
+
+            /* If timestamps are enabled, tcp header will be longer
+             * by 12 bytes, because such header contains Options field.
+             * Options field uses 10 bytes to store timestamps info and
+             * 2 bytes are reserved for other options. If timestamps are
+             * disabled, no options field will be present.
+             */
+            if (disable_timestamps != NULL &&
+                strcmp(disable_timestamps, "yes") == 0 &&
+                sock_type == RPC_SOCK_STREAM)
+            {
+                hsize -= TCP_TIMESTAMPS_HSIZE;
+            }
 
             if (rc - hsize != (int)recv_len)
                 TEST_VERDICT("recvmsg() function returned unexpected "

sockapi-ts/timestamps/timestamps.h
@@ -36,6 +36,9 @@ do {                                            \
 /** MAC address length.  */
 #define LINUX_ETH_HEADER_LEN 14
 
+/** Length of timestamps record in tcp header. */
+#define TCP_TIMESTAMPS_HSIZE 12
+
 /**
  * Structure to keep delays between transmitted packets.
  */

commit 9820ad34cae742cd07009853ac1ec106dcdfd723

timestamps: add extra condition for expected header size
Last patch changing expected header size in case of disabled
timestamps was valid, but not in all testing situations. So new
condition was added to fix it.

Fixes: 25d5e2384 ("timestamps: change expected header size without timestamps")
AMD-Jira-ID: ST-2722
Signed-off-by: Demid Trempolskii <demid.trempolskii@arknetworks.am>
Reviewed-by: Yurij Plotnikov <yurij.plotnikov@arknetworks.am>

sockapi-ts/timestamps/timestamps.c
@@ -392,7 +392,8 @@ ts_check_cmsghdr_addr(rpc_msghdr *msg, int rc, size_t sent_len,
              */
             if (disable_timestamps != NULL &&
                 strcmp(disable_timestamps, "yes") == 0 &&
-                sock_type == RPC_SOCK_STREAM)
+                sock_type == RPC_SOCK_STREAM &&
+                tx)
             {
                 hsize -= TCP_TIMESTAMPS_HSIZE;
             }