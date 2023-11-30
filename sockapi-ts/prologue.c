Xilinx-CNS/cns-sapi-ts

commit 13aec9673ee6a9b5b850ee92e3cd058b15ef6503

prologue: set maximum stacks allowed if applicable
In case of af_xdp stacks can be limited by the number of the combined
channels. When tests are trying to use more stacks than there are,
error occures. So a function to calculate and set max stacks available
for the interface was created.

AMD-Jira-ID: ST-2728
Signed-off-by: Demid Trempolskii <demid.trempolskii@arknetworks.am>
Reviewed-by: Denis Pryazhennikov <denis.pryazhennikov@arknetworks.am>

sockapi-ts/prologue.c
@@ -804,6 +804,66 @@ out:
 }
 
 /**
+ * Set maximum amount stacks possible on the interface based on
+ * combined channels if it's required.
+ *
+ * @return Status code.
+ */
+static te_errno
+set_max_stacks_possible(rcf_rpc_server *rpcs, const char *ifname)
+{
+    te_errno             rc = 0;
+    int                  max_stacks = 0;
+    char                *stacks_limited = getenv("SOCKTS_MAX_STACKS_LIMITED");
+    int                  imp_stacks = 0;
+    te_string            max_stacks_s = TE_STRING_INIT_STATIC(256);
+
+    /* Max amount of stacks should be set only when stacks are limited */
+    if (stacks_limited == NULL || strcmp(stacks_limited, "yes") != 0)
+        return 0;
+
+    rc = cfg_get_int32(&max_stacks,
+                       "/agent:%s/interface:%s/channels:/combined:/current:",
+                       rpcs->ta, ifname);
+    if (rc != 0)
+    {
+        ERROR("Failed to get combined channels: %r", rc);
+        return rc;
+    }
+    if (max_stacks <= 0)
+    {
+        RING_VERDICT("The stack number is expected to be limited by "
+                     "combined channels amount, but combined channels are "
+                     "not supported on this interface or its amount is 0");
+        return 0;
+    }
+
+    rc = te_string_append(&max_stacks_s, "%d", max_stacks);
+    if (rc != 0)
+    {
+        ERROR("Failed to convert max_stack value "
+              "to its string representation: %r", rc);
+        return rc;
+    }
+
+    rc = tapi_sh_env_get_int(rpcs, "SOCKTS_MAX_STACKS", &imp_stacks);
+    if (rc != 0 || imp_stacks > max_stacks)
+    {
+        rc = tapi_sh_env_set(rpcs, "SOCKTS_MAX_STACKS",
+                             max_stacks_s.ptr, TRUE, FALSE);
+
+        if (rc != 0)
+        {
+            ERROR("Failed to set SOCKTS_MAX_STACKS instance %s: %r",
+                  rpcs->ta, rc);
+            return rc;
+        }
+    }
+
+    return 0;
+}
+
+/**
  * Set Socket API library names for Test Agents in accordance
  * with configuration in configurator.conf.
  *
@@ -915,6 +975,9 @@ main(int argc, char **argv)
     TEST_GET_PCO(pco_iut);
     TEST_GET_PCO(pco_tst);
     TEST_GET_IF(iut_if);
+
+    CHECK_RC(set_max_stacks_possible(pco_iut, iut_if->if_name));
+
     pco_gw = tapi_env_get_pco(&env, "pco_gw");
     if (pco_gw == NULL)
         TEST_GET_ADDR(pco_tst, tst_addr);

commit 1c7fcbee1dac3a53fc84ceb7086be22f13e8498c

prologue: use correct ifname in set_max_stacks_possible()
set_max_stacks_possible() function should use parent interface and in case of
netns should use parent agent to obtain correct combined channels number.

AMD-Jira-ID: ST-2728
Fixes: 13aec9673ee6 ("prologue: set maximum stacks allowed if applicable")
Signed-off-by: Yurij Plotnikov <yurij.plotnikov@arknetworks.am>
Reviewed-by: Denis Pryazhennikov <denis.pryazhennikov@arknetworks.am>

sockapi-ts/prologue.c
@@ -817,14 +817,20 @@ set_max_stacks_possible(rcf_rpc_server *rpcs, const char *ifname)
     char                *stacks_limited = getenv("SOCKTS_MAX_STACKS_LIMITED");
     int                  imp_stacks = 0;
     te_string            max_stacks_s = TE_STRING_INIT_STATIC(256);
+    tqe_string          *phys_iface;
+    tqh_strings          phys_ifaces = TAILQ_HEAD_INITIALIZER(phys_ifaces);
+    const char          *ta = sockts_get_used_agt_name(rpcs, ifname);
 
     /* Max amount of stacks should be set only when stacks are limited */
     if (stacks_limited == NULL || strcmp(stacks_limited, "yes") != 0)
         return 0;
 
+    sockts_find_parent_if(rpcs, ifname, &phys_ifaces);
+    phys_iface = TAILQ_FIRST(&phys_ifaces);
+
     rc = cfg_get_int32(&max_stacks,
                        "/agent:%s/interface:%s/channels:/combined:/current:",
-                       rpcs->ta, ifname);
+                       ta, phys_iface->v);
     if (rc != 0)
     {
         ERROR("Failed to get combined channels: %r", rc);