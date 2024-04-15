Xilinx-CNS/cns-sapi-ts

commit 0d84a4bec27e70163e26b6dfeae4e2162d2e7cd5

lib: use proper function for reading big amount of data
sockts_check_blocking() function used rpc_read_fd() to read data from
socket after overfilling and it fails sometimes because of too long
buffer. Let's use rpc_drain_fd_simple() function.

AMD-Jira-ID: ST-2709
Signed-off-by: Yurij Plotnikov <yurij.plotnikov@arknetworks.am>
Reviewed-by: Denis Pryazhennikov <denis.pryazhennikov@arknetworks.am>

sockapi-ts/lib/sockapi-ts.c
@@ -3117,13 +3117,9 @@ sockts_check_blocking(rcf_rpc_server *pco_iut, rcf_rpc_server *pco_tst,
     {
         if (is_send)
         {
-            void *buf = NULL;
-            size_t num = 0;
+            uint64_t num = 0;
 
-            rpc_read_fd(pco_tst, tst_fd, TAPI_WAIT_NETWORK_DELAY, 0, &buf,
-                        &num);
-
-            free(buf);
+            rpc_drain_fd_simple(pco_tst, tst_fd, &num);
         }
         else
             rpc_write(pco_tst, tst_fd, data_buf, data_size);

commit f8f3a584f526f7d737fc51cf46bef820e280b6fb

lib: use correct function to drain pipe
In sockts_check_blocking() function drain_fd() function should be used
in socket case, bacause read_fd() can't read to big amount of data and
read_fd() should be used in case of pipe, because drain_fd() returns
ENOTSOCK in this case.

AMD-Jira-ID: ST-2709
Fixes: 0d84a4bec27e ("lib: use proper function for reading big amount of data")
Signed-off-by: Yurij Plotnikov <yurij.plotnikov@arknetworks.am>
Reviewed-by: Denis Pryazhennikov <denis.pryazhennikov@arknetworks.am>

sockapi-ts/lib/sockapi-ts.c
@@ -3117,9 +3117,31 @@ sockts_check_blocking(rcf_rpc_server *pco_iut, rcf_rpc_server *pco_tst,
     {
         if (is_send)
         {
-            uint64_t num = 0;
+            int optval;
+            int rc;
 
-            rpc_drain_fd_simple(pco_tst, tst_fd, &num);
+            RPC_AWAIT_IUT_ERROR(pco_tst);
+            rc = rpc_getsockopt(pco_tst, tst_fd, RPC_SO_TYPE, &optval);
+            if (rc == -1)
+            {
+                void *buf = NULL;
+                size_t num = 0;
+
+                if (RPC_ERRNO(pco_tst) != RPC_ENOTSOCK)
+                {
+                    TEST_VERDICT("getsockopt(SO_TYPE) returned -1, but set "
+                                 "errno to %r", RPC_ERRNO(pco_tst));
+                }
+                rpc_read_fd(pco_tst, tst_fd, TAPI_WAIT_NETWORK_DELAY, 0, &buf,
+                            &num);
+                free(buf);
+            }
+            else
+            {
+                uint64_t num = 0;
+
+                rpc_drain_fd_simple(pco_tst, tst_fd, &num);
+            }
         }
         else
             rpc_write(pco_tst, tst_fd, data_buf, data_size);