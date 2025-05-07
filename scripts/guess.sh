Xilinx-CNS/cns-sapi-ts

commit 14841d6f16e5c37f7486a8cb118e09f3508560d7

scripts: add publisher
Signed-off-by: Yurij Plotnikov <yurij.plotnikov@arknetworks.am>

scripts/guess.sh
@@ -33,6 +33,14 @@ done
 export TE_BASE=$(te_realpath ${TE_BASE})
 echo "TE_BASE=${TE_BASE}"
 
+if test -z "${TE_INSTALL}" ; then
+    # If undefined, TE_INSTALL is always inside build dir in our case
+    TE_INSTALL="${GUESS_TOP_DIR}build/inst"
+    export TE_INSTALL
+fi
+
+PATH="${PATH}:${TE_INSTALL}/default/bin"
+export PATH
 
 # SF_TS_CONFDIR
 for loop in once ; do

scripts/publish_logs
@@ -1,0 +1,11 @@
+#!/bin/bash
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2025 OKTET Ltd. All rights reserved.
+#
+# Publish logs from the last testing.
+
+source "$(dirname "$(which "$0")")"/guess.sh
+
+source "${TE_TS_RIGSDIR}/scripts/publish_logs/ts_publish"
+
+tsrigs_publish_do "onload" "$@"

scripts/run.sh
@@ -12,7 +12,10 @@ popd >/dev/null
 
 source "${TE_BASE}/scripts/lib"
 source "${TE_BASE}/scripts/lib.grab_cfg"
+source "${TE_BASE}/scripts/lib.meta"
 source_if_exists "${TE_TS_RIGSDIR}/scripts/lib/grab_cfg_handlers"
+
+TE_RUN_META=no
 
 # Include the file if it really exists - this allows sapi-ts not to break.
 # It seems that the following functions may become unavailable on some
@@ -64,6 +67,8 @@ Options:
                             appropriate actions. For now: do not clear kmemleak
                             on debugging kernels.
   --logs-history=<link>     Link to logs history
+  --meta                    Generate testing metadata
+  --publish                 Publish testing logs to Bublik
 EOF
     call_if_defined grab_cfg_print_help
 
@@ -217,10 +222,32 @@ while test -n "$1" ; do
         --build-only)
             export TE_TS_BUILD_ONLY="yes"
             ;;&
+
+        --meta)
+            TE_RUN_META=yes
+            ;;
+
+        --publish)
+            source "${TE_TS_RIGSDIR}/scripts/publish_logs/ts_publish"
+            pscript="$(tsrigs_publish_get onload)"
+            RUN_OPTS="${RUN_OPTS} --publish=${pscript}"
+
+            # Testing metadata is necessary for publishing
+            TE_RUN_META=yes
+            ;;
+
         *)  RUN_OPTS="${RUN_OPTS} $1" ;;
     esac
     shift 1
 done
+
+if [[ "${TE_RUN_META}" = "yes" ]] ; then
+    te_meta_test_suite "onload"
+
+    te_meta_set Configuration "${cfg}"
+    te_meta_set "status" "DONE"
+    te_meta_set_git "${SF_TS_CONFDIR}" TSCONF
+fi
 
 export ST_IGNORE_NM
 export ST_IGNORE_ZEROCONF
@@ -231,7 +258,9 @@ export ZF_SHIM_RUN=$ZF_SHIM_RUN
 # TODO: remove this when sapi-ts stops relying on Jenkins
 # to generate metadata file. Until then this is needed
 # to avoid overwriting Jenkins metadata file by TE.
-RUN_OPTS="${RUN_OPTS} --no-meta"
+if [[ "${TE_RUN_META}" = "no" ]] ; then
+    RUN_OPTS="${RUN_OPTS} --no-meta"
+fi
 
 if test "${ZF_SHIM_RUN}" = "true" ; then
     check_sf_zetaferno_dir $OOL_SET

commit de41e311e91ef682d8458a677db1cd6b36bdd10b

scripts: fix guessing of env variable
Fix guessing of TE_INSTALL.

Fixes: 14841d6f16e5 ("scripts: add publisher")
Signed-off-by: Yurij Plotnikov <yurij.plotnikov@arknetworks.am>
Reviewed-by: Roman Zhukov <roman.zhukov@arknetworks.am>

scripts/guess.sh
@@ -33,9 +33,16 @@ done
 export TE_BASE=$(te_realpath ${TE_BASE})
 echo "TE_BASE=${TE_BASE}"
 
+
+if test -z "${TE_BUILD}" ; then
+    TE_BUILD="${GUESS_TOP_DIR}/build"
+    mkdir -p build
+    export TE_BUILD
+fi
+
 if test -z "${TE_INSTALL}" ; then
     # If undefined, TE_INSTALL is always inside build dir in our case
-    TE_INSTALL="${GUESS_TOP_DIR}build/inst"
+    TE_INSTALL="${TE_BUILD}/inst"
     export TE_INSTALL
 fi
 