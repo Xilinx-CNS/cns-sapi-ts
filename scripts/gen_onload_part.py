Xilinx-CNS/cns-sapi-ts

commit 52fbf769fd9b89ee99e1b8ec076ab0ac0d57473d

scripts/gen_onload_part: enable scalable testing on XF
Enable all but scalable_active_passive.

Signed-off-by: Yurij Plotnikov <yurij.plotnikov@arknetworks.am>
Reviewed-by: Viacheslav Galaktionov <viacheslav.galaktionov@arknetworks.am>

scripts/gen_onload_part.py
@@ -244,12 +244,10 @@ def fix_testing_parms(host, ools, reqs, sl, slice_name,
     if host in params["xf_host"]:
         xf_deny_list = ["fw_low_latency", "fw_full_featured"]
 
+        # ON-16845
         xf_deny_list += ["rss_scalable",
                          "scalable_active_passive",
-                         "one_cluster",
                          ]
-        xf_deny_list += ["scalable", "scalable_active", "scalable_passive"]
-        xf_deny_list += ["scalable_iut", "scalable_any"]
 
         xf_deny_list += ["af_xdp_no_filters", "af_xdp", "zc_af_xdp"]
 

commit cefaaac4bd443b710709b6091d5665bd192f75e9

scripts/gen_onload_part: fix scalable generation on XF
Fixes: 52fbf769fd9b ("scripts/gen_onload_part: enable scalable testing on XF")
Signed-off-by: Yurij Plotnikov <yurij.plotnikov@arknetworks.am>
Reviewed-by: Viacheslav Galaktionov <viacheslav.galaktionov@arknetworks.am>

scripts/gen_onload_part.py
@@ -251,6 +251,9 @@ def fix_testing_parms(host, ools, reqs, sl, slice_name,
 
         xf_deny_list += ["af_xdp_no_filters", "af_xdp", "zc_af_xdp"]
 
+        if "rss_scalable" in ools or "scalable_active_passive" in ools:
+            remove_silent(ools, "scalable_any", "scalable_iut")
+
         for item in xf_deny_list:
             remove_silent(ools, item)
 