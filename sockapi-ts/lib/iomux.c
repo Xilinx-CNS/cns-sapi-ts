Xilinx-CNS/cns-sapi-ts

commit 29799743cdffb01e8bb9255a6e0da570b78b3a20

lib/iomux: implement epoll_pwait2
Implement the case IC_EPOLL_PWAIT2 in function iomux_epoll_call and
related functions.

Signed-off-by: Boris Shleyfman <bshleyfman@oktet.co.il>
Reviewed-by: Yurij Plotnikov <yurij.plotnikov@arknetworks.am>
Reviewed-by: Viacheslav Galaktionov <viacheslav.galaktionov@arknetworks.am>

sockapi-ts/lib/iomux.c
@@ -145,6 +145,11 @@ iomux_call_epoll_internal(iomux_call_type call_type, rcf_rpc_server *rpcs,
                                  epoll_timeout, sigmask);
         break;
 
+        case IC_EPOLL_PWAIT2:
+            rc = rpc_epoll_pwait2(rpcs, cur_st_p->epfd, p_events, n_evts,
+                                  timeout, sigmask);
+        break;
+
         case IC_OO_EPOLL:
         {
             rpc_onload_ordered_epoll_event *oo_events;
@@ -358,7 +363,7 @@ iomux_call_gen(iomux_call_type call_type, rcf_rpc_server *rpcs,
         return rc;
     }
     else if (call_type == IC_EPOLL || call_type == IC_EPOLL_PWAIT ||
-             call_type == IC_OO_EPOLL)
+             call_type == IC_EPOLL_PWAIT2 || call_type == IC_OO_EPOLL)
     {
         rc = iomux_call_epoll_internal(call_type, rpcs, events, n_evts,
                                        timeout, sigmask, save_op);
@@ -835,6 +840,9 @@ int
 iomux_epoll_call(iomux_call_type call_type, rcf_rpc_server *rpcs, int epfd,
                  struct rpc_epoll_event *events, int maxevents, int timeout)
 {
+    struct tarpc_timespec  tv;
+    struct tarpc_timespec *tv_ptr = &tv;
+
     if (call_type == IC_DEFAULT)
         call_type = iomux_call_get_default();
 
@@ -844,6 +852,7 @@ iomux_epoll_call(iomux_call_type call_type, rcf_rpc_server *rpcs, int epfd,
             return rpc_epoll_wait(rpcs, epfd, events, maxevents, timeout);
 
         case IC_EPOLL_PWAIT:
+        case IC_EPOLL_PWAIT2:
         {
             int rc = 0;
             rcf_rpc_op save_op = rpcs->op;
@@ -872,8 +881,20 @@ iomux_epoll_call(iomux_call_type call_type, rcf_rpc_server *rpcs, int epfd,
                 rpcs->iut_err_jump = iut_err_jump;
             }
 
-            rc = rpc_epoll_pwait(rpcs, epfd, events, maxevents, timeout,
-                                 cur_st_p->iomux_call_sigmask);
+            if (call_type == IC_EPOLL_PWAIT)
+            {
+                rc = rpc_epoll_pwait(rpcs, epfd, events, maxevents, timeout,
+                                     cur_st_p->iomux_call_sigmask);
+            }
+            else
+            {
+                if (timeout < 0)
+                    tv_ptr = NULL;
+                else
+                    TE_NS2TS(TE_MS2NS(timeout), &tv);
+                rc = rpc_epoll_pwait2(rpcs, epfd, events, maxevents, tv_ptr,
+                                      cur_st_p->iomux_call_sigmask);
+            }
             duration = rpcs->duration;
             rpc_errno = rpcs->_errno;
             if (save_op != RCF_RPC_CALL &&

sockapi-ts/lib/iomux.h
@@ -39,18 +39,20 @@
     { "ppoll", (int)IC_PPOLL }, \
     { "epoll", (int)IC_EPOLL }, \
     { "epoll_pwait", (int)IC_EPOLL_PWAIT }, \
+    { "epoll_pwait2", (int)IC_EPOLL_PWAIT2 }, \
     { "oo_epoll", (int)IC_OO_EPOLL }
 
 /* Poll and select differ.  Let's find what kind of iomux it is. */
 #define IOMUX_IS_POLL_LIKE(iomux) \
     ((iomux) == IC_POLL || (iomux) == IC_PPOLL || (iomux) == IC_EPOLL || \
-     (iomux) == IC_EPOLL_PWAIT || (iomux) == IC_OO_EPOLL)
+     (iomux) == IC_EPOLL_PWAIT || (iomux) == IC_EPOLL_PWAIT2 || \
+     (iomux) == IC_OO_EPOLL)
 #define IOMUX_IS_SELECT_LIKE(iomux) \
     ((iomux) == IC_SELECT || (iomux) == IC_PSELECT)
-/* Check for pselect, ppoll, epoll_pwait. */
+/* Check for pselect, ppoll, epoll_pwait, epoll_pwait2. */
 #define IOMUX_IS_P_IOMUX(iomux) \
     ((iomux) == IC_PSELECT || (iomux) == IC_PPOLL || \
-     (iomux) == IC_EPOLL_PWAIT)
+     (iomux) == IC_EPOLL_PWAIT || (iomux) == IC_EPOLL_PWAIT2)
 
 /**
  * Get the value of parameter of type 'tapi_iomux_type'
@@ -119,8 +121,8 @@ typedef struct {
 } iomux_evt_fd;
 
 /** 
- * Call 'iomux' function - select, pselect, poll, ppoll, epoll_wait and
- * epoll_pwait - with specified events, timeout and signal
+ * Call 'iomux' function - select, pselect, poll, ppoll, epoll_wait, epoll_pwait
+ * and epoll_pwait2 - with specified events, timeout and signal
  * This function does not process quantity of events greater then
  * standard system macro FD_SETSIZE.
  *
@@ -193,6 +195,7 @@ typedef enum function_type_e {
     FUNCTION_TYPE_PPOLL = TAPI_IOMUX_PPOLL,
     FUNCTION_TYPE_EPOLL = TAPI_IOMUX_EPOLL,
     FUNCTION_TYPE_EPOLL_PWAIT = TAPI_IOMUX_EPOLL_PWAIT,
+    FUNCTION_TYPE_EPOLL_PWAIT2 = TAPI_IOMUX_EPOLL_PWAIT2,
     FUNCTION_TYPE_OO_EPOLL = TAPI_IOMUX_OO_EPOLL,
     FUNCTION_TYPE_RECV
 } function_type_t;
@@ -200,14 +203,15 @@ typedef enum function_type_e {
 /**
  * The list of values allowed for parameter of type 'function_type_t'
  */
-#define FUNCTION_TYPE_MAPPING_LIST              \
-    {"select", FUNCTION_TYPE_SELECT},           \
-    {"pselect", FUNCTION_TYPE_PSELECT},         \
-    {"poll", FUNCTION_TYPE_POLL},               \
-    {"ppoll", FUNCTION_TYPE_PPOLL},             \
-    {"epoll", FUNCTION_TYPE_EPOLL},             \
-    {"epoll_pwait", FUNCTION_TYPE_EPOLL_PWAIT}, \
-    {"oo_epoll", FUNCTION_TYPE_OO_EPOLL},       \
+#define FUNCTION_TYPE_MAPPING_LIST                    \
+    {"select", FUNCTION_TYPE_SELECT},                 \
+    {"pselect", FUNCTION_TYPE_PSELECT},               \
+    {"poll", FUNCTION_TYPE_POLL},                     \
+    {"ppoll", FUNCTION_TYPE_PPOLL},                   \
+    {"epoll", FUNCTION_TYPE_EPOLL},                   \
+    {"epoll_pwait", FUNCTION_TYPE_EPOLL_PWAIT},       \
+    {"epoll_pwait2", FUNCTION_TYPE_EPOLL_PWAIT2}, \
+    {"oo_epoll", FUNCTION_TYPE_OO_EPOLL},             \
     {"recv", FUNCTION_TYPE_RECV}
 
 /**
@@ -316,8 +320,8 @@ iomux_common_steps(iomux_call_type iomux, rcf_rpc_server *iut, int iut_s,
            te_bool fill_buffer, rcf_rpc_server *tst, int tst_s,
            rpc_shut_how how, int *iomux_ret_val);
 
-/* Call epoll_wait() or epoll_pwait() function. All arguments as in
- * epoll_wait() function.
+/* Call epoll_wait(), epoll_pwait() or epoll_pwait2() function. All
+ * arguments as in epoll_wait() function.
  */
 extern int
 iomux_epoll_call(iomux_call_type call_type, rcf_rpc_server *rpcs, int epfd,

commit fe77d195ec9c6478668c9b69cc9973ee2dce5bb6

lib/iomux: use correct struct for time in EPOLL2 call
There was a mistakenly used struct tarpc_timeval instead of
struct tarpc_timespec.
This error makes impossible to build SAPI-TS on Debian 13.

Fixes: 29799743cdff ("lib/iomux: implement epoll_pwait2")
Signed-off-by: Nikolai Kosovskii <nikolai.kosovskii@arknetworks.am>
Reviewed-by: Yurij Plotnikov <yurij.plotnikov@arknetworks.am>

sockapi-ts/lib/iomux.c
@@ -88,6 +88,8 @@ iomux_call_epoll_internal(iomux_call_type call_type, rcf_rpc_server *rpcs,
 {
     struct rpc_epoll_event *p_events;
     int epoll_timeout;
+    struct tarpc_timespec timeout_timespec;
+    struct tarpc_timespec *p_timeout_timespec;
     int rc = 0;
 
     VERB("Epoll requested");
@@ -116,11 +118,46 @@ iomux_call_epoll_internal(iomux_call_type call_type, rcf_rpc_server *rpcs,
         }
     }
 
-    if (timeout != NULL)
-        epoll_timeout = timeout->tv_sec * 1000 + timeout->tv_usec / 1000;
-    else
-        epoll_timeout = -1;
-    VERB("Epoll timeout %d", epoll_timeout);
+    switch (call_type)
+    {
+        case TAPI_IOMUX_EPOLL:
+        case TAPI_IOMUX_EPOLL_PWAIT:
+        case TAPI_IOMUX_OO_EPOLL:
+            if (timeout != NULL)
+            {
+                epoll_timeout = TE_SEC2MS(timeout->tv_sec) +
+                                TE_US2MS(timeout->tv_usec);
+            }
+            else
+            {
+                epoll_timeout = -1;
+            }
+
+            VERB("Epoll timeout %dms", epoll_timeout);
+            break;
+
+        case TAPI_IOMUX_EPOLL_PWAIT2:
+            if (timeout != NULL)
+            {
+                timeout_timespec.tv_sec = timeout->tv_sec;
+                timeout_timespec.tv_nsec = TE_US2NS(timeout->tv_usec);
+                p_timeout_timespec = &timeout_timespec;
+            }
+            else
+            {
+                p_timeout_timespec = NULL;
+            }
+            VERB("Epoll_pwait2 timeout (timespec) %s",
+                 tarpc_timespec2str(p_timeout_timespec));
+            break;
+
+        case TAPI_IOMUX_DEFAULT:
+            ERROR("%s can't be used with default iomux call", __FUNCTION__);
+            break;
+
+        default:
+            ERROR("Wrong type of epoll function");
+    }
 
     rpcs->op = save_op;
     RPC_AWAIT_IUT_ERROR(rpcs);
@@ -146,7 +183,7 @@ iomux_call_epoll_internal(iomux_call_type call_type, rcf_rpc_server *rpcs,
 
         case TAPI_IOMUX_EPOLL_PWAIT2:
             rc = rpc_epoll_pwait2(rpcs, cur_st_p->epfd, p_events, n_evts,
-                                  timeout, sigmask);
+                                  p_timeout_timespec, sigmask);
         break;
 
         case TAPI_IOMUX_OO_EPOLL: