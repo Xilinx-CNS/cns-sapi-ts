7189404dad3a460ac43ff095270a05cf5c871aec
Nikolai Kosovskii <Nikolai.Kosovskii@arknetworks.am>
2025-10-16 18:21:50 +0400

    lib/sockapi-ts: add API to check and receive all sent data
    Signed-off-by: Nikolai Kosovskii <nikolai.kosovskii@arknetworks.am>
    Reviewed-by: Yurij Plotnikov <yurij.plotnikov@arknetworks.am>

--- sockapi-ts/lib/sockapi-ts.c
+++ sockapi-ts/lib/sockapi-ts.c
@@ -3897,3 +3897,22 @@ sockts_destroy_netns(const char *ta, rcf_rpc_server *rpcs_ns,
 
     CHECK_RC(cfg_synchronize_fmt(TRUE, "/agent:%s", netns_ta));
 }
+
+void
+sockts_read_check_fd(rcf_rpc_server *rpcs, int socket, char *tx_buf,
+                     char *rx_buf, size_t exp_len)
+{
+    ssize_t rc_ret;
+    ssize_t len;
+
+    rc_ret = rpc_read_fd(rpcs, socket, TAPI_WAIT_NETWORK_DELAY, exp_len,
+                         &rx_buf, &len);
+
+    if (rc_ret != 0)
+    {
+        TEST_VERDICT("Last recv() failed on %s with %r instead of "
+                     "returning 0", rpcs->name, RPC_ERRNO(rpcs));
+    }
+
+    SOCKTS_CHECK_RECV(rpcs, tx_buf, rx_buf, exp_len, len);
+}
