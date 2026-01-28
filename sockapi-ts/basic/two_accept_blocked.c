Xilinx-CNS/cns-sapi-ts

commit 7189404dad3a460ac43ff095270a05cf5c871aec

lib/sockapi-ts: add API to check and receive all sent data
Signed-off-by: Nikolai Kosovskii <nikolai.kosovskii@arknetworks.am>
Reviewed-by: Yurij Plotnikov <yurij.plotnikov@arknetworks.am>

sockapi-ts/lib/sockapi-ts.c
@@ -3897,3 +3897,22 @@ sockts_destroy_netns(const char *ta, rcf_rpc_server *rpcs_ns,
 
     CHECK_RC(cfg_synchronize_fmt(TRUE, "/agent:%s", netns_ta));
 }
+
+void
+sockts_read_check_fd(rcf_rpc_server *rpcs, int socket, char *tx_buf,
+                     char *rx_buf, size_t exp_len)
+{
+    ssize_t rc_ret;
+    ssize_t len;
+
+    rc_ret = rpc_read_fd(rpcs, socket, TAPI_WAIT_NETWORK_DELAY, exp_len,
+                         &rx_buf, &len);
+
+    if (rc_ret != 0)
+    {
+        TEST_VERDICT("Last recv() failed on %s with %r instead of "
+                     "returning 0", rpcs->name, RPC_ERRNO(rpcs));
+    }
+
+    SOCKTS_CHECK_RECV(rpcs, tx_buf, rx_buf, exp_len, len);
+}

sockapi-ts/lib/sockapi-ts.h
@@ -4154,6 +4154,19 @@ extern void
 sockts_destroy_netns(const char *ta, rcf_rpc_server *rpcs_ns,
                      const char *netns, const char *netns_ta);
 
+/**
+ * Receive all data from the socket and check the data.
+ *
+ * @param rpcs       RPC server handle.
+ * @param socket     Socket.
+ * @param tx_buf     Buffer with sent data
+ * @param rx_buf     Buffer for received data
+ * @param exp_len    Expected data length and
+ *                   length of buffer for received data.
+ */
+extern void sockts_read_check_fd(rcf_rpc_server *rpcs, int socket,
+                                 char *tx_buf, char *rx_buf, size_t exp_len);
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif

commit 1bc6a86b7c8287481c255d4bc8f29353e06ef7af

lib/sockapi-ts: remove RX buff arg from read-check FD func
We don't need this argument outside the function. Also it removes the
problem that SAPI-TS cannot be built on Debian-13 as an engine, see the
link.

Link: https://github.com/Xilinx-CNS/cns-sapi-ts/issues/143
Fixes: 7189404dad3a ("lib/sockapi-ts: add API to check and receive all sent data")
Signed-off-by: Nikolai Kosovskii <nikolai.kosovskii@arknetworks.am>

sockapi-ts/basic/two_accept_blocked.c
@@ -94,10 +94,8 @@ main(int argc, char *argv[])
     te_bool                 iut_aux_done = FALSE;
     te_bool                 do_fork = FALSE;
 
-    uint8_t                 tx_buf[TST_BUF_LEN] = { 0, };
-    uint8_t                 rx_buf[TST_BUF_LEN] = { 0, };
+    char                    tx_buf[TST_BUF_LEN] = { 0, };
     int                     sent;
-    int                     rcv;
 
     TEST_START;
     TEST_GET_PCO(pco_iut);
@@ -187,9 +185,9 @@ main(int argc, char *argv[])
     RPC_SEND(sent, pco_tst1, tst1_s, tx_buf, TST_BUF_LEN, 0);
 
     if (iut1_done)
-        sockts_read_check_fd(pco_iut, acc1_s, tx_buf, rx_buf, TST_BUF_LEN);
+        sockts_read_check_fd(pco_iut, acc1_s, tx_buf, TST_BUF_LEN);
     else
-        sockts_read_check_fd(iut_aux, acc2_s, tx_buf, rx_buf, TST_BUF_LEN);
+        sockts_read_check_fd(iut_aux, acc2_s, tx_buf, TST_BUF_LEN);
 
     rpc_connect(pco_tst2, tst2_s, iut_addr);
 

sockapi-ts/ifcfg/if_up_down_influence.c
@@ -82,7 +82,6 @@ main(int argc, char *argv[])
     int                    rcv2_s = -1;
 
     void     *tx_buf;
-    void     *rx_buf;
 
     te_bool                change_if = FALSE;
 
@@ -108,7 +107,6 @@ main(int argc, char *argv[])
     TEST_GET_BOOL_PARAM(change_if);
 
     tx_buf = te_make_buf_by_len(TST_BUF_LEN);
-    rx_buf = te_make_buf_by_len(TST_BUF_LEN);
 
     GEN_CONNECTION(pco_tst1, pco_iut1, RPC_SOCK_STREAM, RPC_PROTO_DEF,
                    tst1_addr, iut1_addr, &tst1_s, &iut1_s);
@@ -143,12 +141,11 @@ main(int argc, char *argv[])
 
     RPC_SEND(rc, pco_snd1, snd1_s, tx_buf, TST_BUF_LEN, 0);
     MSLEEP(500);
-    sockts_read_check_fd(pco_rcv1, rcv1_s, tx_buf, rx_buf, TST_BUF_LEN);
+    sockts_read_check_fd(pco_rcv1, rcv1_s, tx_buf, TST_BUF_LEN);
 
-    memset(rx_buf, 0, TST_BUF_LEN);
     RPC_SEND(rc, pco_snd2, snd2_s, tx_buf, TST_BUF_LEN, 0);
     MSLEEP(500);
-    sockts_read_check_fd(pco_rcv2, rcv2_s, tx_buf, rx_buf, TST_BUF_LEN);
+    sockts_read_check_fd(pco_rcv2, rcv2_s, tx_buf, TST_BUF_LEN);
 
     pco_snd1->op = RCF_RPC_CALL;
     rpc_simple_sender(pco_snd1, snd1_s, 1, 10, 0, 0, 10000, 1, 20, &sent, 0);
@@ -160,10 +157,9 @@ main(int argc, char *argv[])
     CHECK_RC(tapi_cfg_base_if_down(pco_iut1->ta, if_to_shutdown->if_name));
     CFG_WAIT_CHANGES;
 
-    memset(rx_buf, 0, TST_BUF_LEN);
     RPC_SEND(rc, pco_snd2, snd2_s, tx_buf, TST_BUF_LEN, 0);
     MSLEEP(500);
-    sockts_read_check_fd(pco_rcv2, rcv2_s, tx_buf, rx_buf, TST_BUF_LEN);
+    sockts_read_check_fd(pco_rcv2, rcv2_s, tx_buf, TST_BUF_LEN);
 
     /* Call simple reciever to get number of bytes received */
     rpc_simple_receiver(pco_rcv1, rcv1_s, 0, &received_before);
@@ -199,6 +195,5 @@ cleanup:
     CLEANUP_RPC_CLOSE(pco_tst2, tst2_s);
 
     free(tx_buf);
-    free(rx_buf);
     TEST_END;
 }

sockapi-ts/lib/sockapi-ts.c
@@ -3900,10 +3900,11 @@ sockts_destroy_netns(const char *ta, rcf_rpc_server *rpcs_ns,
 
 void
 sockts_read_check_fd(rcf_rpc_server *rpcs, int socket, char *tx_buf,
-                     char *rx_buf, size_t exp_len)
+                     size_t exp_len)
 {
-    ssize_t rc_ret;
-    ssize_t len;
+    int rc_ret;
+    size_t len;
+    void *rx_buf = NULL;
 
     rc_ret = rpc_read_fd(rpcs, socket, TAPI_WAIT_NETWORK_DELAY, exp_len,
                          &rx_buf, &len);
@@ -3914,5 +3915,10 @@ sockts_read_check_fd(rcf_rpc_server *rpcs, int socket, char *tx_buf,
                      "returning 0", rpcs->name, RPC_ERRNO(rpcs));
     }
 
+    if (len > exp_len)
+        TEST_VERDICT("Received data has %zd length; that is bigger "
+                     "than expected %zd length", len, exp_len);
+
     SOCKTS_CHECK_RECV(rpcs, tx_buf, rx_buf, exp_len, len);
+    free(rx_buf);
 }

sockapi-ts/lib/sockapi-ts.h
@@ -4159,13 +4159,11 @@ sockts_destroy_netns(const char *ta, rcf_rpc_server *rpcs_ns,
  *
  * @param rpcs       RPC server handle.
  * @param socket     Socket.
- * @param tx_buf     Buffer with sent data
- * @param rx_buf     Buffer for received data
- * @param exp_len    Expected data length and
- *                   length of buffer for received data.
+ * @param tx_buf     Buffer with sent data.
+ * @param exp_len    Expected data length.
  */
 extern void sockts_read_check_fd(rcf_rpc_server *rpcs, int socket,
-                                 char *tx_buf, char *rx_buf, size_t exp_len);
+                                 char *tx_buf, size_t exp_len);
 
 #ifdef __cplusplus
 } /* extern "C" */

sockapi-ts/tcp/connection_timeout_data.c
@@ -53,7 +53,6 @@ main(int argc, char *argv[])
     tapi_route_gateway gateway;
 
     char   *sndbuf = NULL;
-    char   *rcvbuf = NULL;
     int iut_s = -1;
     int tst_s = -1;
 
@@ -73,7 +72,6 @@ main(int argc, char *argv[])
     CHECK_RC(rcf_rpc_server_restart(pco_iut));
 
     sndbuf = te_make_buf_by_len(DATA_SIZE);
-    rcvbuf = te_make_buf_by_len(DATA_SIZE);
 
     TEST_STEP("Configure gateway.");
     TAPI_INIT_ROUTE_GATEWAY(gateway);
@@ -152,7 +150,7 @@ main(int argc, char *argv[])
         TEST_VERDICT("Connection has not been dropped");
 
     TEST_STEP("Read and check all data on IUT.");
-    sockts_read_check_fd(pco_iut, iut_s, sndbuf, rcvbuf, DATA_SIZE);
+    sockts_read_check_fd(pco_iut, iut_s, sndbuf, DATA_SIZE);
 
     TEST_SUCCESS;
 cleanup:
@@ -160,7 +158,6 @@ cleanup:
     CLEANUP_RPC_CLOSE(pco_tst, tst_s);
 
     free(sndbuf);
-    free(rcvbuf);
 
     TEST_END;
 }

sockapi-ts/tcp/tcp_loopback.c
@@ -128,8 +128,7 @@ main(int argc, char *argv[])
     uint16_t    iut_port;
     te_bool     v6only;
 
-    uint8_t     tx_buf[BUF_SIZE];
-    uint8_t     rx_buf[BUF_SIZE];
+    char tx_buf[BUF_SIZE];
 
     int             sid;
     unsigned int    received_packets_number = 0;
@@ -205,7 +204,6 @@ main(int argc, char *argv[])
     }
 
     te_fill_buf(tx_buf, BUF_SIZE);
-    memset(rx_buf, 0, BUF_SIZE);
 
     SA(&loopback_addr)->sa_family = SA(iut_addr1)->sa_family;
     te_sockaddr_set_loopback(SA(&loopback_addr));
@@ -448,12 +446,10 @@ main(int argc, char *argv[])
                   "csap id %d", rc, csap);
 
     rpc_send(pco_iut, acc_s, tx_buf, BUF_SIZE, 0);
-    sockts_read_check_fd(pco_tst, tst_s, tx_buf, rx_buf, BUF_SIZE);
-
-    memset(rx_buf, 0, BUF_SIZE);
+    sockts_read_check_fd(pco_tst, tst_s, tx_buf, BUF_SIZE);
 
     rpc_send(pco_tst, tst_s, tx_buf, BUF_SIZE, 0);
-    sockts_read_check_fd(pco_iut, acc_s, tx_buf, rx_buf, BUF_SIZE);
+    sockts_read_check_fd(pco_iut, acc_s, tx_buf, BUF_SIZE);
 
     if (tapi_tad_trrecv_stop(pco_iut->ta, sid, csap, NULL,
                              &received_packets_number))